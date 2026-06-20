b1863df0c01fdfce76551818327b822dbad67bb8
PhucLuu410 <luuphuc20012001@gmail.com>
2026-06-20 16:12:56 +0700

    refactor Firmware Boot

--- Firmware_Boot/Load_Firmware.cpp
+++ Firmware_Boot/Load_Firmware.cpp
@@ -2,28 +2,34 @@
 #include <iostream>
 #include <windows.h>
 #include <string>
-#include <vector>
 
 int main()
 {
-    std::string filePath = "C:\\Users\\luuph\\OneDrive\\Desktop\\New folder\\Firmware_Application\\system\\build\\project.bin";
+    std::string filePath = "C:\\Users\\luuph\\OneDrive\\Desktop\\ROBOT_ARM\\Firmware_Application\\system\\build\\project.bin";
 
     // 1. Mở cổng COM
-    HANDLE hSerial = CreateFileA("\\\\.\\COM5", GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+    HANDLE hSerial = CreateFileA("\\\\.\\COM5", GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
     if (hSerial == INVALID_HANDLE_VALUE)
     {
         std::cerr << "Khong mo duoc COM5!" << std::endl;
         return 1;
     }
 
+    // 2. Cấu hình Serial
     DCB dcb = {0};
     dcb.DCBlength = sizeof(dcb);
     GetCommState(hSerial, &dcb);
-    dcb.BaudRate = CBR_9600;
+    dcb.BaudRate = CBR_9600; // Lưu ý: 9600 rất chậm, truyền file lớn sẽ lâu
     dcb.ByteSize = 8;
     dcb.Parity = NOPARITY;
     dcb.StopBits = ONESTOPBIT;
     SetCommState(hSerial, &dcb);
+
+    COMMTIMEOUTS timeouts = {0};
+    timeouts.ReadIntervalTimeout = 50;
+    timeouts.ReadTotalTimeoutConstant = 5000;
+    timeouts.ReadTotalTimeoutMultiplier = 10;
+    SetCommTimeouts(hSerial, &timeouts);
 
     std::ifstream binFile(filePath, std::ios::binary);
     if (!binFile.is_open())
@@ -33,18 +39,48 @@ int main()
         return 1;
     }
 
-    std::cout << "Bat dau truyen du lieu..." << std::endl;
-    DWORD bytesWritten;
     char buffer[16];
+    DWORD bytesWritten, bytesRead;
 
     while (binFile.read(buffer, 16) || binFile.gcount() > 0)
     {
         WriteFile(hSerial, buffer, binFile.gcount(), &bytesWritten, NULL);
-        Sleep(30);
+        std::cout << "Da gui " << bytesWritten << " byte. Dang cho ACK..." << std::endl;
+        bool receivedAck = false;
+        int timeoutCounter = 0;
+        char ackBuffer[1];
+
+        while (!receivedAck)
+        {
+            if (ReadFile(hSerial, ackBuffer, 1, &bytesRead, NULL))
+            {
+                if (bytesRead > 0 && ackBuffer[0] == 0x06)
+                {
+                    std::cout << "Nhan duoc ACK. Tiep tuc..." << std::endl;
+                    receivedAck = true;
+                }
+            }
+            else
+            {
+                std::cerr << "Loi ket noi Serial!" << std::endl;
+                binFile.close();
+                CloseHandle(hSerial);
+                return -1;
+            }
+
+            Sleep(10);
+            timeoutCounter++;
+            if (timeoutCounter > 500)
+            {
+                std::cerr << "Timeout! STM32 khong phan hoi." << std::endl;
+                binFile.close();
+                CloseHandle(hSerial);
+                return -1;
+            }
+        }
     }
 
     std::cout << "Da truyen xong file!" << std::endl;
-
     binFile.close();
     CloseHandle(hSerial);
     return 0;
--- Firmware_Boot/main.c
+++ Firmware_Boot/main.c
@@ -16,8 +16,7 @@ typedef enum
 
 volatile uint8_t currentByte = 0;
 volatile uint16_t current_buffer = 0;
-volatile uint32_t current_addr = APP_ADDR;
-volatile uint8_t flag = 0;
+volatile uint8_t Update_Flag = 0;
 volatile uint8_t count = 0;
 
 uint8_t Data_Buffer[16] = {0};
@@ -25,6 +24,7 @@ uint8_t Start_Buffer[3] = {0};
 volatile BootState bootState = BOOT_IDLE_STATE;
 uint16_t *pLength = NULL;
 uint32_t initialFileLength = 0;
+volatile uint8_t Index = 0;
 
 static void delay(volatile uint32_t d)
 {
@@ -32,39 +32,50 @@ static void delay(volatile uint32_t d)
         ;
 }
 
-void Flash_Init(void)
+void System_Init(void)
 {
-    FLASH->ACR |= (1 << 4);
-    while (!(FLASH->ACR & (1 << 5)))
+    RCC->CR |= (1 << 0);
+    while (!(RCC->CR & (1 << 1)))
         ;
-    FLASH->KEYR = 0x45670123;
-    FLASH->KEYR = 0xCDEF89AB;
+    RCC->APB2ENR |= (1 << 0) | (1 << 2) | (1 << 4) | (1 << 9) | (1 << 14);
+
+    GPIOA->CRL = 0x00000000;
+    GPIOA->CRH = 0x00000000;
+    GPIOA->CRL |= (1 << 16) | (1 << 17) | (1 << 18);
+    GPIOA->CRH |= (0x0B << 4) | (8 << 8);
 }
 
-void Flash_ErasePage(uint32_t Address, uint32_t NumberPage)
+void Flash_Init(void)
+{
+    FLASH->KEYR = 0x45670123;
+    FLASH->KEYR = 0xCDEF89AB;
+    while (FLASH->CR & FLASH_CR_LOCK)
+        ;
+}
+
+void Flash_ErasePage(uint32_t Address)
 {
     while (FLASH->SR & (1 << 0))
         ;
     FLASH->CR |= (1 << 1);
-    for (int i = 0; i < NumberPage; i++)
-    {
-        FLASH->AR = Address + (i * 1024);
-        FLASH->CR |= FLASH_CR_STRT;
-        while (FLASH->SR & (1 << 0))
-            ;
-    }
+    FLASH->AR = Address;
+    FLASH->CR |= (1 << 6);
+
+    while (FLASH->SR & (1 << 0))
+        ;
     FLASH->CR &= ~(1 << 1);
 }
 
 void Flash_WriteHalfWord(uint32_t Address, uint16_t Data)
 {
-    while (FLASH->SR & (1 << 0))
-        ;
-    FLASH->CR |= (1 << 0);
-    *(volatile uint16_t *)Address = Data;
-    while (FLASH->SR & (1 << 0))
-        ;
-    FLASH->CR &= ~(1 << 0);
+    if (!(FLASH->SR & (1 << 0)))
+    {
+        FLASH->CR |= (1 << 0);
+        *(volatile uint16_t *)Address = Data;
+        while (FLASH->SR & (1 << 0))
+            ;
+        FLASH->CR &= ~(1 << 0);
+    }
 }
 
 void Uart_Init(void)
@@ -82,33 +93,8 @@ void Uart_SendData(uint8_t data)
     USART1->DR = data;
 }
 
-void System_Init(void)
-{
-    RCC->CR |= (1 << 0);
-    while (!(RCC->CR & (1 << 1)))
-        ;
-    RCC->APB2ENR |= (1 << 0) | (1 << 2) | (1 << 4) | (1 << 9) | (1 << 14);
-
-    GPIOA->CRL = 0x00000000;
-    GPIOA->CRH = 0x00000000;
-    GPIOA->CRL |= (1 << 16) | (1 << 17) | (1 << 18);
-    GPIOA->CRH |= (0x0B << 4) | (8 << 8);
-}
-
 void JumpToApp(void)
 {
-    if (((*(volatile uint32_t *)APP_ADDR) & 0x2FFE0000) == 0x20000000)
-    {
-        uint32_t *App_Psp = (uint32_t *)APP_ADDR;
-        uint32_t *App_Entry = (uint32_t *)(APP_ADDR + 4);
-
-        __disable_irq();
-        USART1->CR1 &= ~(1 << 13);
-
-        __set_MSP(*App_Psp);
-        void (*App_Jump)(void) = (void (*)(void))(*App_Entry);
-        App_Jump();
-    }
 }
 
 int main(void)
@@ -116,53 +102,23 @@ int main(void)
     System_Init();
     Flash_Init();
     Uart_Init();
-    Flash_ErasePage(APP_ADDR, 60);
-
-    currentByte = 0;
-    bootState = BOOT_IDLE_STATE;
-    count = 0;
-    pLength = (uint16_t *)&Start_Buffer[1];
-
+    Index = 0;
+    Update_Flag = 0;
+    for (int i = 0; i < 16; i++)
+    {
+        Data_Buffer[i] = 0;
+    }
+    for (int i = 0; i < 60; i++)
+    {
+        Flash_ErasePage(APP_ADDR + (0x400 * i));
+    }
     while (1)
     {
-        if (bootState == BOOT_IDLE_STATE)
+        if (Update_Flag == 1)
         {
-            Uart_SendData(0x28);
-            bootState = BOOT_WAIT_START_STATE;
-        }
-
-        if (bootState == BOOT_SEND_START_STATE)
-        {
-            *pLength = ((*pLength & 0x00FF) << 8) | ((*pLength & 0xFF00) >> 8);
-            initialFileLength = *pLength;
-
-            Uart_SendData(0x02);
-            bootState = BOOT_RECEIVE_STATE;
-        }
-
-        if (bootState == BOOT_CHECK_DATA_STATE)
-        {
-            uint8_t halfWordsToWrite = (currentByte + 1) / 2;
-
-            for (int i = 0; i < halfWordsToWrite; i++)
+            for (int i = 0; i < 8; i++)
             {
-                uint16_t Data = (Data_Buffer[2 * i + 1] << 8) | Data_Buffer[2 * i];
-                Flash_WriteHalfWord(current_addr, Data);
-                current_addr += 2;
-            }
-
-            currentByte = 0;
-
-            if (*pLength == 0)
-            {
-                Uart_SendData(0x04);
-                delay(100);
-                JumpToApp();
-            }
-            else
-            {
-                bootState = BOOT_RECEIVE_STATE;
-                Uart_SendData(0x01);
+                Flash_WriteHalfWord(APP_ADDR + (0x2 * i), (Data_Buffer[i]) | (Data_Buffer[i + 1] << 8));
             }
         }
     }
@@ -170,33 +126,20 @@ int main(void)
 
 void USART1_IRQHandler(void)
 {
-    if (USART1->SR & (1 << 5))
+    while (!(USART1->SR & (1 << 5)))
+        ;
+    Data_Buffer[Index] = USART1->DR;
+    Index++;
+    if (Index == 16)
     {
-        uint8_t temp_rx = (uint8_t)USART1->DR;
+        Update_Flag = 1;
+        Index = 0;
+    }
+}
 
-        if (bootState == BOOT_WAIT_START_STATE)
-        {
-            Start_Buffer[count++] = temp_rx;
-            if (count == 3)
-            {
-                bootState = BOOT_SEND_START_STATE;
-                count = 0;
-            }
-        }
-
-        else if (bootState == BOOT_RECEIVE_STATE && *pLength > 0)
-        {
-            Data_Buffer[currentByte++] = temp_rx;
-            (*pLength)--;
-
-            if (currentByte == 16 || *pLength == 0)
-            {
-                bootState = BOOT_CHECK_DATA_STATE;
-            }
-            else
-            {
-                Uart_SendData(0x01);
-            }
-        }
+void HardFault_Handler(void)
+{
+    while (1)
+    {
     }
 }
